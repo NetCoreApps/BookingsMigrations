58ca512c928994730417a22adba0084819dce6fe
Darren Reid <darren@reidmail.org>
2022-09-14 14:49:47 +1000

    first commit

---
+++ MyApp/Migrations/Migration1001.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.OrmLite;
+
+namespace MyApp.Migrations;
+
+public class Migration1001 : MigrationBase
+{
+    class Booking
+    {
+        public bool? IsRepeatGuest { get; set; }
+    }
+    
+    public override void Up()
+    {
+        Db.Migrate<Booking>();
+    }
+
+    public override void Down()
+    {
+        Db.Revert<Booking>();
+    }
+}
---
+++ MyApp/Migrations/Migration1002.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace MyApp.Migrations;
+
+public class Migration1002 : MigrationBase
+{
+    public override void Up()
+    {
+        Db.ExecuteSql(
+        @"ALTER TABLE ""Booking"" RENAME COLUMN ""IsRepeatGuest"" TO ""RepeatGuest"";");
+    }
+
+    public override void Down()
+    {
+        Db.ExecuteSql(
+        @"ALTER TABLE ""Booking"" RENAME COLUMN ""RepeatGuest"" TO ""IsRepeatGuest"";");
+    }
+}
